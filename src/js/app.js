22c42b7f2712bbedad33bb4af9cfc668625da1d5
Alexandr Varygin <otz-job@mail.ru>
2020-07-22 20:49:01 +0300

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import GameSavingLoader from './GameSavingLoader';
+
+GameSavingLoader.load().then(
+  (saving) => {
+    // saving объект класса GameSaving
+    console.log(saving);
+  },
+  (error) => {
+    // ...
+    console.log(error);
+  },
+);
